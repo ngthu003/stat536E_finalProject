adf264b112fca4178b68f9e32a868493072bd174
Thu Nguyen <ngthu003@gmail.com>
2023-04-15 00:43:40 -0700

    Fixed fn.DGP, added fn.Complete.Case.Analysis and main.script

---
+++ 01.Code/main.script.R
@@ -0,0 +1,89 @@
+
+##############################################################################|
+#
+# Stat 536E - Final Project
+# - Author: Thu Nguyen
+# - Topic:  MI - Join MVN vs. Condition
+#
+##############################################################################|
+
+
+
+# ============================================================================!
+# 0) Libraries ----------------------------------------------------------------
+# ----------------------------------------------------------------------------!
+libs <- c('tidyverse', 'mi', 'arm', 'Amelia', 'mvnmle', 'MASS')
+lapply(libs, require, character.only = TRUE)
+rm(libs)
+
+
+# ============================================================================!
+# 1) DGP ----------------------------------------------------------------------
+# ----------------------------------------------------------------------------!
+
+source('01.Code/fn.DGP.R')
+#' @examples
+#' ### A) Fixed --------------------------
+#' tmp.N               <- 10           # #(obs)
+#' tmp.no.Var.Observed <- 3            # #(var. fully observed)
+#' 
+#' ### B) Adjustable ---------------------
+#' tmp.var.Type        <- 'continuous' # Type of variables
+#' tmp.no.Var.Missing  <- 2            # #(var. w/ missing data)
+#' tmp.miss.Rate.X     <- .1           # missing data rate per X explanatory var.
+#' tmp.miss.Rate.Y     <- .25          # missing data rate per Y response var.
+#' tmp.restriction     <- 'triangular' # missing data mechanism
+#' tmp.strong <- 0                     # applicable only when "triangular"
+#' 
+#' tmp.no.Category     <- 3
+#' tmp.imp.Method      <- 'ppd'
+#'
+#' ### C) Other --------------------------
+#' tmp.estimate.CPCs <- F              # F to save time, default is T
+
+## Continuous ---------------------------------------------
+dgp.cts <- fn.DGP(
+  N               = tmp.N,
+  var.Type        = tmp.var.Type,
+  no.Category     = tmp.no.Category,
+  no.Var.Observed = tmp.no.Var.Observed,
+  no.Var.Missing  = tmp.no.Var.Missing,
+  miss.Rate.X     = tmp.miss.Rate.X,
+  miss.Rate.Y     = tmp.miss.Rate.Y,
+  restriction     = tmp.restriction,
+  strong          = tmp.strong,
+  imp.Method      = tmp.imp.Method,
+  estimate.CPCs   = tmp.estimate.CPCs)
+
+
+
+## Ordinal ------------------------------------------------
+tmp.var.Type = 'ordinal'
+dgp.ord <- fn.DGP(
+  N               = tmp.N,
+  var.Type        = tmp.var.Type,
+  no.Category     = tmp.no.Category,
+  no.Var.Observed = tmp.no.Var.Observed,
+  no.Var.Missing  = tmp.no.Var.Missing,
+  miss.Rate.X     = tmp.miss.Rate.X,
+  miss.Rate.Y     = tmp.miss.Rate.Y,
+  restriction     = tmp.restriction,
+  strong          = tmp.strong,
+  imp.Method      = tmp.imp.Method,
+  estimate.CPCs   = tmp.estimate.CPCs)
+
+rm(list = setdiff(ls(), c('fn.DGP', 'dgp.cts', 'dgp.ord')))
+
+
+
+
+# ============================================================================!
+# 2) Complete Case Analysis ---------------------------------------------------
+# ----------------------------------------------------------------------------!
+
+source('01.Code/fn.Complete.Case.Analysis.R')
+
+cca.cts <- fn.Complete.Case.Analysis(dgp.cts)
+data.frame(t(sapply(cca.cts,c)))
+cca.ord <- fn.Complete.Case.Analysis(dgp.ord)
+data.frame(t(sapply(cca.ord,c)))
