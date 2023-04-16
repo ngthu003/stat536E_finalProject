ea0b6e6f01e3d83fcd9a1bd6fb2abca4c2cf29b2
Thu Nguyen <ngthu003@gmail.com>
2023-04-16 00:59:32 -0700

    Updated main.script to run test simulation

--- 01.Code/main.script.R
+++ 01.Code/main.script.R
@@ -12,114 +12,243 @@
 # ============================================================================!
 # 0) Libraries ----------------------------------------------------------------
 # ----------------------------------------------------------------------------!
-libs <- c('tidyverse', 'mi', 'arm', 'Amelia', 'mvnmle', 'MASS', 'norm2', 'nnet')
+libs <- c('tidyverse', 'mi', 'arm', 'Amelia', 'mvnmle', 'MASS', 'norm2', 'norm', 'nnet')
 lapply(libs, require, character.only = TRUE)
 rm(libs)
 
 
 
 # ============================================================================!
-# 1) DGP ----------------------------------------------------------------------
+# 1) Helper fns ---------------------------------------------------------------
 # ----------------------------------------------------------------------------!
 
+# Generate random data
 source('01.Code/fn.DGP.R')
-#' @examples
-#' ### A) Fixed --------------------------
-#' tmp.N               <- 40           # #(obs)
-#' tmp.no.Var.Observed <- 3            # #(var. fully observed)
-#' 
-#' ### B) Adjustable ---------------------
-#' tmp.var.Type        <- 'continuous' # Type of variables
-#' tmp.no.Var.Missing  <- 2            # #(var. w/ missing data)
-#' tmp.miss.Rate.X     <- .1           # missing data rate per X explanatory var.
-#' tmp.miss.Rate.Y     <- .25          # missing data rate per Y response var.
-#' tmp.restriction     <- 'triangular' # missing data mechanism
-#' tmp.strong <- 0                     # applicable only when "triangular"
-#' 
-#' tmp.no.Category     <- 3
-#' tmp.imp.Method      <- 'pmm'
-#'
-#' ### C) Other --------------------------
-#' tmp.estimate.CPCs <- F              # F to save time, default is T
-
-## Continuous ---------------------------------------------
-dgp.cts <- fn.DGP(
-  N               = tmp.N,
-  var.Type        = tmp.var.Type,
-  no.Category     = tmp.no.Category,
-  no.Var.Observed = tmp.no.Var.Observed,
-  no.Var.Missing  = tmp.no.Var.Missing,
-  miss.Rate.X     = tmp.miss.Rate.X,
-  miss.Rate.Y     = tmp.miss.Rate.Y,
-  restriction     = tmp.restriction,
-  strong          = tmp.strong,
-  imp.Method      = tmp.imp.Method,
-  estimate.CPCs   = tmp.estimate.CPCs)
-
-
-
-## Ordinal ------------------------------------------------
-tmp.var.Type = 'ordinal'
-dgp.ord <- fn.DGP(
-  N               = tmp.N,
-  var.Type        = tmp.var.Type,
-  no.Category     = tmp.no.Category,
-  no.Var.Observed = tmp.no.Var.Observed,
-  no.Var.Missing  = tmp.no.Var.Missing,
-  miss.Rate.X     = tmp.miss.Rate.X,
-  miss.Rate.Y     = tmp.miss.Rate.Y,
-  restriction     = tmp.restriction,
-  strong          = tmp.strong,
-  imp.Method      = tmp.imp.Method,
-  estimate.CPCs   = tmp.estimate.CPCs)
-
-rm(list = setdiff(ls(), c('fn.DGP', 'dgp.cts', 'dgp.ord')))
-
-
-
-
-# ============================================================================!
-# 2) Complete Case Analysis ---------------------------------------------------
-# ----------------------------------------------------------------------------!
-
+# Complete Case Analysis
 source('01.Code/fn.Complete.Case.Analysis.R')
-
-cca.cts <- fn.Complete.Case.Analysis(dgp.cts)
-data.frame(t(sapply(cca.cts,c)))
-cca.ord <- fn.Complete.Case.Analysis(dgp.ord)
-data.frame(t(sapply(cca.ord,c)))
-
-
-
-
-
-
-# ============================================================================!
-# 3) Conditional MI Analysis --------------------------------------------------
-# ----------------------------------------------------------------------------!
-
+# MI: 1) Conditional MI
 source('01.Code/fn.Conditional.MI.R')
-
-m <- 3
-cond.MI.cts <- fn.Conditional.MI(dgp.cts, m = m)
-data.frame(t(sapply(cond.MI.cts,c)))
-cond.MI.ord <- fn.Conditional.MI(dgp.ord, m = m, no.Category = dgp.ord$dgp.Settings$no.Category)
-data.frame(t(sapply(cond.MI.ord,c)))
-
-
-
-
-
-# ============================================================================!
-# 3) Joint MVN MI Analysis ----------------------------------------------------
-# ----------------------------------------------------------------------------!
-
+# MI: 2) Joint MI
 source('01.Code/fn.Joint.MVN.MI.helper.PMM.R')
 source('01.Code/fn.Joint.MVN.MI.R')
 
 
-m <- 3
-joint.MI.amelia.cts <- fn.Joint.MVN.MI(dgp.cts, m = m, command = 'amelia')
-data.frame(t(sapply(joint.MI.amelia.cts,c)))
-joint.MI.amelia.ord <- fn.Joint.MVN.MI(dgp.ord, m = m, command = 'amelia')
-data.frame(t(sapply(joint.MI.amelia.ord,c)))
+
+
+# ============================================================================!
+# 2) Hyper-Parameters ---------------------------------------------------------
+# ----------------------------------------------------------------------------!
+
+
+## Fixed --------------------------------------------------
+global.N               <- 20  #(obs)
+global.no.Var.Observed <- 5    #(var. fully observed)
+global.no.Category     <- 5    #(categories) per discrete var.
+global.strong          <- 0    # applicable only when "triangular"
+global.estimate.CPCs   <- F    # F to save time, default is T
+global.no.Imputation   <- 5    # m, the number of imputation per 1 dataset
+
+
+## Adjustable ---------------------------------------------
+
+### A) Methodology --------------------
+Options.imp.Method     <- c('ppd', 'pmm')
+
+### B) Scenario -----------------------
+Options.restriction     <- c('triangular', 'none')
+Options.var.Type        <- c('continuous', 'ordinal')
+Options.no.Var.Missing  <- seq(1, 5, 2)
+Options.miss.Rate.X     <- seq(.1, .3, .1)
+Options.miss.Rate.Y     <- c(.25, .5)
+Options.analysis.Method <- c(
+  'Complete_Case_Analysis',
+  'Joint_MVN_MI_Amelia',
+  'Joint_MVN_MI_Norm',
+  'Conditional_MI'
+)
+
+
+
+
+# ============================================================================!
+# 3) Sim. Tracking DF ---------------------------------------------------------
+# ----------------------------------------------------------------------------!
+
+overall.Results <- data.frame(
+  Iteration = integer()
+  # Adjustable hyper-parameters
+  , restriction    = character()
+  , var.Type       = character()
+  , no.Var.Missing = integer()
+  , miss.Rate.Y    = numeric()
+  , miss.Rate.X    = numeric()
+  # Fied hyper-parameters
+  , N               = integer()
+  , no.Var.Observed = integer()
+  , no.Category     = integer()
+  , no.Imputation   = integer()
+  # Analysis method
+  , analysis.Method = character()
+  , imp.Method      = character()
+  # Results
+  # -- Acc: Imputed values
+  , Imputed.Values.RMSE = numeric()
+  , Imputed.Values.Bias = numeric()
+  # -- Acc: Choice probabilities
+  , Choice.Prob.RMSE = numeric()
+  , Choice.Prob.Bias = numeric()
+  # -- Acc: Coefficients
+  , mdl.X1.coef.RMSE = numeric()
+  , mdl.X1.coef.Bias = numeric()
+  , mdl.X1.coef.MNS  = numeric()
+  , mdl.Y.coef.RMSE  = numeric()
+  , mdl.Y.coef.Bias  = numeric()
+  , mdl.Y.coef.MNS   = numeric()
+  # -- Acc: Fitted values
+  , mdl.X1.fitted.RMSE     = numeric()
+  , mdl.X1.fitted.Bias     = numeric()
+  , mdl.X1.fitted.All.RMSE = numeric()
+  , mdl.X1.fitted.All.Bias = numeric()
+  , mdl.Y.fitted.RMSE      = numeric()
+  , mdl.Y.fitted.Bias      = numeric()
+  , mdl.Y.fitted.All.RMSE  = numeric()
+  , mdl.Y.fitted.All.Bias  = numeric()
+  # -- Time to Convergence
+  , time.to.MI.Convergence = numeric()
+  , time.to.MI.Analyze     = numeric()
+)
+
+fn.null.to.NA <- function(x) {ifelse(is.null(x), NA, x)}
+
+
+
+# ============================================================================!
+# 4) Simulations --------------------------------------------------------------
+# ----------------------------------------------------------------------------!
+
+counter <- 1
+no.Iteration <- 1
+
+## 0: Iteration -------------------------------------------
+for (iter in 1:no.Iteration) {
+  
+  ## 1: Missing Mechanism -----------------------------------
+  for (tmp.restriction in Options.restriction) {
+    
+    ## 2: Var. Type -----------------------------------------
+    for (tmp.var.Type in Options.var.Type) {
+      
+      ## 3: No. Var. Missing --------------------------------
+      for (tmp.no.Var.Missing in Options.no.Var.Missing) {
+        
+        ## 4: Miss Rate Y -----------------------------------
+        for (tmp.miss.Rate.Y in Options.miss.Rate.Y) {
+          
+          ## 5: Miss Rate X ---------------------------------
+          for (tmp.miss.Rate.X in Options.miss.Rate.X) {
+            
+            ## 6: imp.Method --------------------------------
+            for (tmp.imp.Method in c('ppd', 'pmm')) {
+            
+              ## =================================================================!
+              ### 91: Simulate Data -----------------------
+              dgp <- fn.DGP(
+                N               = global.N,
+                var.Type        = tmp.var.Type,
+                no.Category     = global.no.Category,
+                no.Var.Observed = global.no.Var.Observed,
+                no.Var.Missing  = tmp.no.Var.Missing,
+                miss.Rate.X     = tmp.miss.Rate.X,
+                miss.Rate.Y     = tmp.miss.Rate.Y,
+                restriction     = tmp.restriction,
+                strong          = global.strong,
+                imp.Method      = tmp.imp.Method)
+              
+              ## =================================================================!
+              ## 92: Analyze w. diff. Methods -------------
+              for (tmp.analysis.Method in Options.analysis.Method) {
+
+                ## =================================================================!
+                ### A) Analyze ============================
+                time.Start   <- proc.time()
+                if (tmp.analysis.Method == 'Complete_Case_Analysis') {
+                  results <- fn.Complete.Case.Analysis(dgp)
+                  # --------------------------------!
+                } else if (tmp.analysis.Method == 'Joint_MVN_MI_Amelia') {
+                  results <- fn.Joint.MVN.MI(dgp, m = global.no.Imputation, command = 'amelia')
+                  # --------------------------------!
+                } else if (tmp.analysis.Method == 'Joint_MVN_MI_Norm') {
+                  results <- fn.Joint.MVN.MI(dgp, m = global.no.Imputation, command = 'norm')
+                  # --------------------------------!
+                } else if (tmp.analysis.Method == 'Conditional_MI') {
+                  results <- fn.Conditional.MI(dgp, m = global.no.Imputation)
+                  # --------------------------------!
+                } else {
+                  ##### Else ------------------------|
+                  stop('analysis.Method is not recognized.')
+                }
+                time.End        <- proc.time()
+                time.to.Analyze <- as.numeric((time.End - time.Start)[3])
+
+                ## =================================================================!
+                ### B) Add to Tracking Table ================
+                overall.Results[nrow(overall.Results)+1,] <- c(
+                  iter
+                  # Adjustable hyper-parameters
+                  , tmp.restriction
+                  , tmp.var.Type
+                  , tmp.no.Var.Missing
+                  , tmp.miss.Rate.Y
+                  , tmp.miss.Rate.X
+                  # Fied hyper-parameters
+                  , global.N
+                  , global.no.Var.Observed
+                  , global.no.Category
+                  , global.no.Imputation
+                  # Analysis method
+                  # , 'Complete_Case_Analysis'
+                  , tmp.analysis.Method
+                  , tmp.imp.Method
+                  # Results
+                  # -- Acc: Imputed values
+                  , fn.null.to.NA(results$Imputed.Values.RMSE)
+                  , fn.null.to.NA(results$Imputed.Values.Bias)
+                  # -- Acc: Choice probabilities
+                  , fn.null.to.NA(results$Choice.Prob.RMSE)
+                  , fn.null.to.NA(results$Choice.Prob.Bias)
+                  # -- Acc: Coefficients
+                  , fn.null.to.NA(results$mdl.X1.coef.RMSE)
+                  , fn.null.to.NA(results$mdl.X1.coef.Bias)
+                  , fn.null.to.NA(results$mdl.X1.coef.MNS)
+                  , fn.null.to.NA(results$mdl.Y.coef.RMSE)
+                  , fn.null.to.NA(results$mdl.Y.coef.Bias)
+                  , fn.null.to.NA(results$mdl.Y.coef.MNS)
+                  # -- Acc: Fitted values
+                  , fn.null.to.NA(results$mdl.X1.fitted.RMSE)
+                  , fn.null.to.NA(results$mdl.X1.fitted.Bias)
+                  , fn.null.to.NA(results$mdl.X1.fitted.All.RMSE)
+                  , fn.null.to.NA(results$mdl.X1.fitted.All.Bias)
+                  , fn.null.to.NA(results$mdl.Y.fitted.RMSE)
+                  , fn.null.to.NA(results$mdl.Y.fitted.Bias)
+                  , fn.null.to.NA(results$mdl.Y.fitted.All.RMSE)
+                  , fn.null.to.NA(results$mdl.Y.fitted.All.Bias)
+                  # -- Time to Convergence
+                  , fn.null.to.NA(results$time)
+                  , time.to.Analyze
+                )
+                
+                counter <- counter+1
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+
+
+
+
+
