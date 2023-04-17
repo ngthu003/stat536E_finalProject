1dce1a1288b24b4ee2f2e9db5ea5c35f63a5a628
Thu Nguyen <ngthu003@gmail.com>
2023-04-17 01:29:57 -0700

    Added script to prepare plots

---
+++ 01.Code/script.plots.R
@@ -0,0 +1,216 @@
+
+##############################################################################|
+#
+# Stat 536E - Final Project
+# - Author: Thu Nguyen
+# - Topic:  MI - Join MVN vs. Condition
+#
+##############################################################################|
+
+
+
+# ============================================================================!
+# 0) Libraries ----------------------------------------------------------------
+# ----------------------------------------------------------------------------!
+libs <- c('dplyr', 'ggplot2', 'ggthemes', 'ggpubr', 'reshape2')
+lapply(libs, require, character.only = TRUE)
+rm(libs)
+
+
+
+
+
+# ============================================================================!
+# 1) Load Simulation Data -----------------------------------------------------
+# ----------------------------------------------------------------------------!
+
+load('01.Code/overall.Results.RData')
+
+
+
+colnames(overall.Results)
+
+
+# ============================================================================!
+# 2) fn -----------------------------------------------------------------------
+# ----------------------------------------------------------------------------!
+
+fn.plot.restriction <- function(tmp.var.Type, 
+                                tmp.evaluation.Metric, label.evaluation.Metric,
+                                tmp.imp.Method, legend.text.Indicator) {
+  # Get data
+  tmp <- overall.Results %>% 
+    mutate(
+      `Missing Mechanism` = ifelse(restriction == 'triangular', 'MAR', 'NMAR')
+    ) %>% 
+    filter(
+      miss.Rate.Y    == .25,
+      miss.Rate.X    == .1,
+      no.Var.Missing == 3,
+    ) %>% 
+    filter(
+      imp.Method == tmp.imp.Method,
+      var.Type   == tmp.var.Type
+    ) %>% 
+    group_by(
+      `Missing Mechanism`,
+      analysis.Method
+    ) %>% 
+    summarize(
+      value = mean(!!sym(tmp.evaluation.Metric))
+    )
+  # Plot
+  p <- tmp %>% 
+    ggplot() +
+    geom_bar(aes(x = analysis.Method, y = value, fill = analysis.Method),
+             width = 0.75, position = 'dodge', stat = 'identity') +
+    xlab('Missing Mechanism') +
+    ylab(label.evaluation.Metric) +
+    labs(title = label.evaluation.Metric) +
+    guides(color = 'none') + 
+    scale_fill_brewer(palette = "Set2",
+                      direction = -1,
+                      name = 'Missing Mechanism',
+                      labels = c('Complete Case Analysis', 'Conditional MI',
+                                 'Joint MVN MI - Amelia', 'Joint MVN MI - norm')) +
+    theme(plot.background    = element_rect(fill = 'white'),
+          panel.background   = element_rect(fill = 'white'),
+          panel.grid.major   = element_line(linetype = 'dashed',
+                                            size = .5, color = 'lightgray'),
+          axis.text.x = element_blank(),
+          # Legend info
+          legend.background = element_rect(fill = "white"),
+          legend.position   = 'bottom',
+          legend.title      = element_blank()
+    ) +
+    facet_wrap(~`Missing Mechanism`) +
+    theme(panel.spacing.x = unit(2, "lines"))
+  return(p)
+}
+
+
+fn.plot.restriction(
+  tmp.var.Type   = 'continuous',
+  tmp.imp.Method = 'ppd',
+  # tmp.evaluation.Metric = 'Imputed.Values.RMSE',
+  tmp.evaluation.Metric = 'mdl.Y.coef.RMSE',
+  label.evaluation.Metric = 'Model Coefficients - RMSE',
+  legend.text.Indicator = 'Yes'
+)
+
+fn.plot.restriction(
+  tmp.var.Type   = 'continuous',
+  tmp.imp.Method = 'ppd',
+  # tmp.evaluation.Metric = 'Imputed.Values.RMSE',
+  tmp.evaluation.Metric = 'mdl.Y.coef.Bias',
+  label.evaluation.Metric = 'Model Coefficients - Bias',
+  legend.text.Indicator = 'Yes'
+)
+
+
+
+
+
+colnames(overall.Results)
+
+# ============================================================================!
+# 3) Plots --------------------------------------------------------------------
+# ----------------------------------------------------------------------------!
+
+Options.value.Type <- c(
+  'Imputed Values', 'Choice Probabilities',
+  'GLM Coefficients', 'GLM Fitted Values'
+)
+
+
+
+# ========================================================!
+## MAR vs. NMAR -------------------------------------------
+imp.Method <- 'ppd'
+
+
+
+
+### PPD-Continuous --------------------
+var.Type   <- 'continuous'
+
+
+for (value.Type in Options.value.Type) {
+  
+  if (value.Type == 'Imputed Values') {
+    evaluation.Metric <- 'Imputed.Values'
+  } else if (value.Type == 'Choice Probabilities') {
+    evaluation.Metric <- 'Choice.Prob'
+  } else if (value.Type == 'GLM Coefficients') {
+    evaluation.Metric <- 'mdl.Y.coef'
+  } else if (value.Type == 'GLM Fitted Values') {
+    evaluation.Metric <- 'mdl.Y.fitted.All'
+  } else {
+    stop('value.Type not recognized.')
+  }
+  
+  p_RMSE <- fn.plot.restriction(
+    tmp.var.Type            = var.Type,
+    tmp.imp.Method          = imp.Method,
+    tmp.evaluation.Metric   = paste0(evaluation.Metric, '.RMSE'),
+    label.evaluation.Metric = paste0(value.Type, ' - RMSE'),
+    legend.text.Indicator   = 'Yes'
+  )
+  p_Bias <- fn.plot.restriction(
+    tmp.var.Type            = var.Type,
+    tmp.imp.Method          = imp.Method,
+    tmp.evaluation.Metric   = paste0(evaluation.Metric, '.Bias'),
+    label.evaluation.Metric = paste0(value.Type, ' - Bias'),
+    legend.text.Indicator   = 'Yes'
+  )  
+  p <- ggarrange(plotlist = list(p_RMSE, p_Bias), 
+                 ncol = 1, common.legend = TRUE , legend = "bottom")
+  png(paste0('03.Figures/', var.Type, '.', imp.Method, '.', gsub(' ', '\\.', value.Type), '.png'), 
+      width = 800, height = 400, pointsize = 15)
+  print(p)
+  dev.off()
+}
+
+
+
+
+### PPD-Ordinal -----------------------
+var.Type   <- 'ordinal'
+
+for (value.Type in Options.value.Type) {
+  
+  if (value.Type == 'Imputed Values') {
+    evaluation.Metric <- 'Imputed.Values'
+  } else if (value.Type == 'Choice Probabilities') {
+    evaluation.Metric <- 'Choice.Prob'
+  } else if (value.Type == 'GLM Coefficients') {
+    evaluation.Metric <- 'mdl.Y.coef'
+  } else if (value.Type == 'GLM Fitted Values') {
+    evaluation.Metric <- 'mdl.Y.fitted.All'
+  } else {
+    stop('value.Type not recognized.')
+  }
+  
+  p_RMSE <- fn.plot.restriction(
+    tmp.var.Type            = var.Type,
+    tmp.imp.Method          = imp.Method,
+    tmp.evaluation.Metric   = paste0(evaluation.Metric, '.RMSE'),
+    label.evaluation.Metric = paste0(value.Type, ' - RMSE'),
+    legend.text.Indicator   = 'Yes'
+  )
+  p_Bias <- fn.plot.restriction(
+    tmp.var.Type            = var.Type,
+    tmp.imp.Method          = imp.Method,
+    tmp.evaluation.Metric   = paste0(evaluation.Metric, '.Bias'),
+    label.evaluation.Metric = paste0(value.Type, ' - Bias'),
+    legend.text.Indicator   = 'Yes'
+  )  
+  p <- ggarrange(plotlist = list(p_RMSE, p_Bias), 
+                 ncol = 1, common.legend = TRUE , legend = "bottom")
+  png(paste0('03.Figures/', var.Type, '.', imp.Method, '.', gsub(' ', '\\.', value.Type), '.png'), 
+      width = 800, height = 400, pointsize = 15)
+  print(p)
+  dev.off()
+}
+
+
