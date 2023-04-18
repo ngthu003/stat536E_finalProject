ce87bfa58f617dbfbf3fbc37a49df8fae20ec99f
Thu Nguyen <ngthu003@gmail.com>
2023-04-18 01:43:27 -0700

    Added plots

--- 01.Code/script.plots.R
+++ 01.Code/script.plots.R
@@ -277,6 +277,75 @@ fn.plot.Imputed.Values.miss.Rate.Y <- function(
 
 
 
+
+# ========================================================!
+## Miss Rate X --------------------------------------------
+
+fn.plot.Imputed.Values.miss.Rate.X <- function(
+    tmp.imp.Method, tmp.var.Type,
+    tmp.evaluation.Metric, label.evaluation.Metric,
+    tmp.metric.Type = 'RMSE'
+) {
+  # Get data
+  tmp <- overall.Results %>% 
+    # mutate(
+    #   `Missing Mechanism` = ifelse(restriction == 'triangular', 'MAR', 'NMAR')
+    # ) %>% 
+    filter(
+      restriction    == 'triangular',
+      miss.Rate.Y    == .25,
+      # miss.Rate.X    == .1,
+      no.Var.Missing == 3,
+    ) %>% 
+    filter(
+      imp.Method == tmp.imp.Method,
+      var.Type   == tmp.var.Type
+    ) %>% 
+    group_by(
+      miss.Rate.X,
+      analysis.Method
+    ) %>% 
+    summarize(
+      value = mean(!!sym(paste0(tmp.evaluation.Metric, '.', tmp.metric.Type)))
+    )
+  # Plot
+  p <- tmp %>% 
+    ggplot() +
+    geom_bar(aes(x = analysis.Method, y = value, fill = analysis.Method),
+             width = 0.75, position = 'dodge', stat = 'identity') +
+    xlab('Miss Rate in X') +
+    ylab(paste0(label.evaluation.Metric, ' - ', tmp.metric.Type)) +
+    labs(title = paste0(label.evaluation.Metric, ' - ', tmp.metric.Type)) +
+    guides(color = 'none') + 
+    scale_fill_brewer(palette = "Set2",
+                      direction = -1,
+                      name = 'Miss Rate in X, the explanatory variables',
+                      labels = c('Complete Case Analysis', 'Conditional MI',
+                                 'Joint MVN MI - Amelia', 'Joint MVN MI - norm')) +
+    theme(plot.background    = element_rect(fill = 'white'),
+          panel.background   = element_rect(fill = 'white'),
+          panel.grid.major   = element_line(linetype = 'dashed',
+                                            size = .5, color = 'lightgray'),
+          axis.text.x = element_blank(),
+          # Legend info
+          legend.background = element_rect(fill = "white"),
+          legend.position   = 'bottom',
+          legend.title      = element_blank()
+    ) +
+    facet_wrap(~miss.Rate.X) +
+    theme(panel.spacing.x = unit(2, "lines"))
+  # Return plot
+  return(p)
+}
+
+
+
+
+
+
+
+
+
 colnames(overall.Results)
 
 # ============================================================================!
@@ -681,6 +750,203 @@ for (value.Type in Options.value.Type.Ordinal) {
 p <- ggarrange(plotlist = p.list, ncol = 1, heights = c(1, 0.1, 1, .1, 1), 
                common.legend = TRUE, legend = 'bottom')
 png(paste0('03.Figures/02.Miss_Rate_Y/', dimension, '.', var.Type, '.', 
+           imp.Method, '.', metric.Type, '.png'), 
+    width = 500, height = 1000, pointsize = 15)
+print(p)
+dev.off()
+
+
+
+
+
+
+
+# ========================================================!
+## Miss Rate X --------------------------------------------
+
+dimension <- 'Miss_Rate_X'
+
+### RMSE --------------------------------------------------
+metric.Type <- 'RMSE'
+
+
+
+# ==============================================|
+#### PPD:Continuous -----------------------------
+
+var.Type <- 'continuous'
+imp.Method <- 'ppd'
+
+p.list <- list()
+counter <- 0
+for (value.Type in Options.value.Type.Continuous) {
+  counter <- counter + 1
+  # Prepare Metric
+  if (value.Type == 'Imputed Values') {
+    evaluation.Metric <- 'Imputed.Values'
+  } else if (value.Type == 'Choice Probabilities') {
+    evaluation.Metric <- 'Choice.Prob'
+  } else if (value.Type == 'GLM Coefficients') {
+    evaluation.Metric <- 'mdl.Y.coef'
+  } else if (value.Type == 'GLM Fitted Values') {
+    evaluation.Metric <- 'mdl.Y.fitted.All'
+  } else {
+    stop('value.Type not recognized.')
+  }
+  # Plot
+  p <- fn.plot.Imputed.Values.miss.Rate.X(
+    tmp.imp.Method          = imp.Method,
+    tmp.var.Type            = var.Type,
+    tmp.evaluation.Metric   = paste0(evaluation.Metric),
+    label.evaluation.Metric = paste0(value.Type),
+    tmp.metric.Type = metric.Type
+  )
+  p.list[[counter]] <- p
+  counter <- counter+1
+  p.list[[counter]] <- NULL
+}
+
+p <- ggarrange(plotlist = p.list, ncol = 1, heights = c(1, 0.1, 1, .1, 1), 
+               common.legend = TRUE, legend = 'bottom')
+png(paste0('03.Figures/03.Miss_Rate_X/', dimension, '.', var.Type, '.', 
+           imp.Method, '.', metric.Type, '.png'), 
+    width = 500, height = 1000, pointsize = 15)
+print(p)
+dev.off()
+
+
+
+
+# ==============================================|
+#### PPD:Continuous -----------------------------
+
+var.Type <- 'ordinal'
+imp.Method <- 'ppd'
+
+p.list <- list()
+counter <- 0
+for (value.Type in Options.value.Type.Ordinal) {
+  counter <- counter + 1
+  # Prepare Metric
+  if (value.Type == 'Imputed Values') {
+    evaluation.Metric <- 'Imputed.Values'
+  } else if (value.Type == 'Choice Probabilities') {
+    evaluation.Metric <- 'Choice.Prob'
+  } else if (value.Type == 'GLM Coefficients') {
+    evaluation.Metric <- 'mdl.Y.coef'
+  } else if (value.Type == 'GLM Fitted Values') {
+    evaluation.Metric <- 'mdl.Y.fitted.All'
+  } else {
+    stop('value.Type not recognized.')
+  }
+  # Plot
+  p <- fn.plot.Imputed.Values.miss.Rate.X(
+    tmp.imp.Method          = imp.Method,
+    tmp.var.Type            = var.Type,
+    tmp.evaluation.Metric   = paste0(evaluation.Metric),
+    label.evaluation.Metric = paste0(value.Type),
+    tmp.metric.Type = metric.Type
+  )
+  p.list[[counter]] <- p
+  counter <- counter+1
+  p.list[[counter]] <- NULL
+}
+
+p <- ggarrange(plotlist = p.list, ncol = 1, heights = c(1, 0.1, 1, .1, 1), 
+               common.legend = TRUE, legend = 'bottom')
+png(paste0('03.Figures/03.Miss_Rate_X/', dimension, '.', var.Type, '.', 
+           imp.Method, '.', metric.Type, '.png'), 
+    width = 500, height = 1000, pointsize = 15)
+print(p)
+dev.off()
+
+
+
+
+# ==============================================|
+#### PMM:Continuous -----------------------------
+
+var.Type <- 'continuous'
+imp.Method <- 'pmm'
+
+p.list <- list()
+counter <- 0
+for (value.Type in Options.value.Type.Continuous) {
+  counter <- counter + 1
+  # Prepare Metric
+  if (value.Type == 'Imputed Values') {
+    evaluation.Metric <- 'Imputed.Values'
+  } else if (value.Type == 'Choice Probabilities') {
+    evaluation.Metric <- 'Choice.Prob'
+  } else if (value.Type == 'GLM Coefficients') {
+    evaluation.Metric <- 'mdl.Y.coef'
+  } else if (value.Type == 'GLM Fitted Values') {
+    evaluation.Metric <- 'mdl.Y.fitted.All'
+  } else {
+    stop('value.Type not recognized.')
+  }
+  # Plot
+  p <- fn.plot.Imputed.Values.miss.Rate.X(
+    tmp.imp.Method          = imp.Method,
+    tmp.var.Type            = var.Type,
+    tmp.evaluation.Metric   = paste0(evaluation.Metric),
+    label.evaluation.Metric = paste0(value.Type),
+    tmp.metric.Type = metric.Type
+  )
+  p.list[[counter]] <- p
+  counter <- counter+1
+  p.list[[counter]] <- NULL
+}
+
+p <- ggarrange(plotlist = p.list, ncol = 1, heights = c(1, 0.1, 1, .1, 1), 
+               common.legend = TRUE, legend = 'bottom')
+png(paste0('03.Figures/03.Miss_Rate_X/', dimension, '.', var.Type, '.', 
+           imp.Method, '.', metric.Type, '.png'), 
+    width = 500, height = 1000, pointsize = 15)
+print(p)
+dev.off()
+
+
+
+
+# ==============================================|
+#### PMM:Continuous -----------------------------
+
+var.Type <- 'ordinal'
+imp.Method <- 'pmm'
+
+p.list <- list()
+counter <- 0
+for (value.Type in Options.value.Type.Ordinal) {
+  counter <- counter + 1
+  # Prepare Metric
+  if (value.Type == 'Imputed Values') {
+    evaluation.Metric <- 'Imputed.Values'
+  } else if (value.Type == 'Choice Probabilities') {
+    evaluation.Metric <- 'Choice.Prob'
+  } else if (value.Type == 'GLM Coefficients') {
+    evaluation.Metric <- 'mdl.Y.coef'
+  } else if (value.Type == 'GLM Fitted Values') {
+    evaluation.Metric <- 'mdl.Y.fitted.All'
+  } else {
+    stop('value.Type not recognized.')
+  }
+  # Plot
+  p <- fn.plot.Imputed.Values.miss.Rate.X(
+    tmp.imp.Method          = imp.Method,
+    tmp.var.Type            = var.Type,
+    tmp.evaluation.Metric   = paste0(evaluation.Metric),
+    label.evaluation.Metric = paste0(value.Type),
+    tmp.metric.Type = metric.Type
+  )
+  p.list[[counter]] <- p
+  counter <- counter+1
+  p.list[[counter]] <- NULL
+}
+
+p <- ggarrange(plotlist = p.list, ncol = 1, heights = c(1, 0.1, 1, .1, 1), 
+               common.legend = TRUE, legend = 'bottom')
+png(paste0('03.Figures/03.Miss_Rate_X/', dimension, '.', var.Type, '.', 
            imp.Method, '.', metric.Type, '.png'), 
     width = 500, height = 1000, pointsize = 15)
 print(p)
